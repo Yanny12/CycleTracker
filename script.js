abcfb9bd8ab50c607af9fae0a878dcf61c13036c
Jan <99971937+Yanny12@users.noreply.github.com>
2026-04-30 12:12:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
+function berechnen() {
+  let blutung = document.getElementById("blutung").value;
 
+  let phase = "";
+  let risiko = 0;
+
+  if (blutung == 3) {
+    phase = "Menstruation";
+    risiko = 0.1;
+  } else if (blutung == 2) {
+    phase = "frühe Zyklusphase";
+    risiko = 0.3;
+  } else if (blutung == 1) {
+    phase = "mögliche fruchtbare Phase";
+    risiko = 1;
+  } else {
+    phase = "möglicher Eisprung";
+    risiko = 2;
+  }
+
+  // Kupferspirale reduziert stark
+  let mitSpirale = risiko * 0.01;
+
+  document.getElementById("phase").innerText =
+    "Geschätzte Phase: " + phase;
+
+  document.getElementById("wahrscheinlichkeit").innerText =
+    "Schwangerschaftsrisiko: " + mitSpirale.toFixed(2) + "%";
+
+  zeichneDiagramm(mitSpirale);
+}
+
+
+function zeichneDiagramm(wert) {
+  const ctx = document.getElementById('chart');
+
+  new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: ["niedrig", "mittel", "hoch"],
+      datasets: [{
+        label: 'Risiko %',
+        data: [0.1, 1, wert],
+      }]
+    }
+  });
+}
